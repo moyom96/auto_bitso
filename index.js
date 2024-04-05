48295ed643f60091df5bd84666e40b68e6912ec7
Moy Montaño Copca <moises.montano@Moys-MacBook-Pro-2.local>
2024-04-05 10:45:57 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+import fetch from "node-fetch";
+import crypto from "crypto";
+
+import "dotenv/config";
+
+const { API_KEY, API_SECRET } = process.env;
+const API_URL = `https://bitso.com`;
+
+const ENDPOINTS = {
+  balance: "/api/v3/balance/",
+  order: "/api/v3/orders/",
+};
+
+const BOOKS = {
+  btc: "btc_mxn",
+  usd: "usd_mxn",
+};
+
+const MXN_PER_ORDER = "100";
+
+// body has to be stringified
+const callRest = async (method, path, body = "") => {
+  const nonce = Date.now();
+  const data = `${nonce}${method}${path}${body}`;
+  const hmac = crypto.createHmac("sha256", API_SECRET);
+  hmac.update(data);
+  const signature = hmac.digest("hex");
+
+  const response = await fetch(`${API_URL}${path}`, {
+    method,
+    body: body.length > 0 ? body : undefined,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bitso ${API_KEY}:${nonce}:${signature}`,
+    },
+  });
+
+  return response.json();
+};
+
+const getBalances = async () => {
+  const { balances } = (await callRest("GET", ENDPOINTS.balance)).payload;
+
+  return balances.filter(({ total }) => Number(total) > 0);
+};
+
+const placeOrder = async (book) => {
+  const body = {
+    book,
+    minor: MXN_PER_ORDER,
+    side: "buy",
+    type: "market",
+  };
+
+  return callRest("POST", ENDPOINTS.order, JSON.stringify(body));
+};
+
+const balances = await getBalances();
+const { available: mxnBalance } = balances.find(
+  ({ currency }) => currency === "mxn"
+);
+
+console.log(`mxn: ${mxnBalance}`);
+
+const {
+  success: usdSuccess,
+  payload: { oid: usdOid = "" } = { oid: "" },
+  error: usdError,
+} = await placeOrder(BOOKS.usd);
+
+if (usdSuccess) {
+  console.log(`usd-oid: ${usdOid}`);
+} else {
+  console.log(`usd-oid error: ${usdError.code}:${usdError.message}`);
+}
+
+const {
+  success: btcSuccess,
+  payload: { oid: btcOid = "" } = { oid: "" },
+  error: btcError,
+} = await placeOrder(BOOKS.btc);
+
+if (btcSuccess) {
+  console.log(`btc-oid: ${btcOid}`);
+} else {
+  console.log(`btc-oid error: ${btcError.code}:${btcError.message}`);
+}
